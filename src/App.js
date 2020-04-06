67ce158c8ec049ecf1753a0c529bdb59e83aee7e
Chiradeep Vittal <chiradeepv@gmail.com>
2020-04-06 14:12:16 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
+//import Container from 'react-bootstrap/Container';
+//import Row from 'react-bootstrap/Row';
+//import Col from 'react-bootstrap/Col';
 import './App.css';
+import Form from "@rjsf/core"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const schema = {
+  title: "Todo",
+  type: "object",
+  required: ["title"],
+  properties: {
+    title: {type: "string", title: "Title", default: "A new task"},
+    done: {type: "boolean", title: " Done?", default: false}
+  }
+};
+
+const uischema = {
+	"title": {
+           "ui:widget": "textarea"
+	}
+}
+
+const log = (type) => console.log.bind(console, type);
+
+class App extends React.Component {
+	render() {
+	  return (
+		  <div class="col-sm-8">
+		  <div class="row">
+		  <div class="col-sm-6">
+                     <Form schema={schema}
+		           uiSchema={uischema}
+                           onChange={log("changed")}
+                           onSubmit={log("submitted")}
+                           onError={log("errors")} />
+		  </div>
+		  </div>
+		  </div>
+	  );
+	}
 }
 
 export default App;
