786ebb40d23deed2a4f2f6a64daf74d71f84f5cd
Chiradeep Vittal <chiradeepv@gmail.com>
2020-04-06 16:30:05 -0700

    use bootstrap 3

--- src/App.js
+++ src/App.js
@@ -1,42 +1,62 @@
 import React from 'react';
-//import Container from 'react-bootstrap/Container';
-//import Row from 'react-bootstrap/Row';
-//import Col from 'react-bootstrap/Col';
 import './App.css';
 import Form from "@rjsf/core"
 
 
 const schema = {
-  title: "Todo",
-  type: "object",
-  required: ["title"],
   properties: {
-    title: {type: "string", title: "Title", default: "A new task"},
-    done: {type: "boolean", title: " Done?", default: false}
-  }
+		adcSettings: {
+			type: "object",
+			title: "ADC Settings",
+			required: ["nsIp"],
+			properties: {
+				nsIp: {type: "string", title: "Citrix ADC IP"},
+				nsPort: {type: "string", title: "ADC Port",  default: "443"},
+				nsVIP: {type: "string", title: "Virtual IP for the clients to connect to", },
+			}
+		},
+		deploymentSettings: {
+			type: "object",
+			title: "Deployment Settings",
+			properties: {
+				'license.accept': {type: "boolean", title: "Accept License", default: false},
+			}
+		}
+	},
 };
 
 const uischema = {
-	"title": {
-           "ui:widget": "textarea"
+	adcSettings: {
+		"nsIp" : {
+			"ui:help": "Citrix ADC NSIP/SNIP, SNIP in case of HA (mgmt has to be enabled)"
+		}
+	},
+	deploymentSettings: {
+		"license.accept": {
+			"ui:help": "Set to yes to accept the terms and conditions of the Citrix license."
+		} 
 	}
 }
 
 const log = (type) => console.log.bind(console, type);
+const onSubmit = ({formData}, e) => console.log("Data submitted: ",  formData);
+
 
 class App extends React.Component {
 	render() {
 	  return (
-		  <div class="col-sm-8">
-		  <div class="row">
-		  <div class="col-sm-6">
-                     <Form schema={schema}
-		           uiSchema={uischema}
-                           onChange={log("changed")}
-                           onSubmit={log("submitted")}
-                           onError={log("errors")} />
-		  </div>
-		  </div>
+		  <div className="col-sm-8">
+		 		<div className="row">
+				 <div className="col-sm-8">
+				 </div>
+		  		<div className="col-sm-8">
+              <Form schema={schema}
+		            		uiSchema={uischema}
+                    onChange={log("changed")}
+                    onSubmit={onSubmit}
+                    onError={log("errors")} />
+		  		</div>
+		  	</div>
 		  </div>
 	  );
 	}
