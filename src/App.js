feb1738cef5409eabcbe1e7112a1b7d14af0a932
Chiradeep Vittal <chiradeepv@gmail.com>
2020-04-06 19:13:24 -0700

    complete all form fields

--- src/App.js
+++ src/App.js
@@ -13,13 +13,38 @@ const schema = {
 				nsIp: {type: "string", title: "Citrix ADC IP"},
 				nsPort: {type: "string", title: "ADC Port",  default: "443"},
 				nsVIP: {type: "string", title: "Virtual IP for the clients to connect to", },
+				nsProtocol: {type: "string", title: "ADC Management Protocol", default: "HTTPS"},
+				nsNamespace: {type: "string", title: "ADC Entity Prefix"},
+				defaultSSLCert: {type: "string", title: "Default SSLCert"},
+				nodeWatch: {type: "boolean", title: "Node Watch", default: false},
 			}
 		},
 		deploymentSettings: {
 			type: "object",
 			title: "Deployment Settings",
 			properties: {
+				'kubernetesURL': {type: "string", title: "Kubernetes API-server URL"},
 				'license.accept': {type: "boolean", title: "Accept License", default: false},
+				'ingressClass': {type: "string", title: "Ingress Class for the controller"},
+				'logLevel': {type: "string", default: "DEBUG", title: "Ingress Controller Log Level", enum:["DEBUG", "INFO", "WARN", "ERROR", "TRACE"]},
+			}
+		},
+		cicImageSettings: {
+			type: "object",
+			title: "Citrix Ingress Controller Image Settings",
+			properties: {
+				'cic.image': {type: "string", title: "Citrix Ingress Controller Image", default:"quay.io/citrix/citrix-k8s-ingress-controller:1.7.6"},
+				'cic.pullpolicy': {type: "string", default: "IfNotPresent", title: "Image Pull Policy", enum: ["Always", "IfNotPresent", "Never"]}
+			}
+		},
+		exporterSettings: {
+			type: "object",
+			title: "Citrix ADC Metrics Exporter Settings",
+			properties: {
+				'exporter.required': {type: "boolean", title: "Exporter Required", default: true},
+				'exporter.image': {type: "string", title: "Citrix ADC Metrics Exporter Image", default:"quay.io/citrix/citrix-adc-metrics-exporter:1.4.0"},
+				'exporter.pullpolicy': {type: "string", default: "IfNotPresent", title: "Image Pull Policy", enum: ["Always", "IfNotPresent", "Never"]},
+				'exporter.ports.containerPort': {type: "number", title: "Exporter Container Port", default: 8888}
 			}
 		}
 	},
@@ -29,6 +54,15 @@ const uischema = {
 	adcSettings: {
 		"nsIp" : {
 			"ui:help": "Citrix ADC NSIP/SNIP, SNIP in case of HA (mgmt has to be enabled)"
+		},
+		"nsNamespace": {
+			"ui:help": "Resources created on the ADC will have this prefix in their name"
+		},
+		"defaultSSLCert": {
+			"ui:help": "Secret containing the default ceritifcate for SSL vservers"
+		},
+		"nodeWatch": {
+			"ui:help": "When checked, routes on the Citrix ADC are automatically configured to point to the pod networks on the nodes",
 		}
 	},
 	deploymentSettings: {
@@ -39,7 +73,7 @@ const uischema = {
 }
 
 const log = (type) => console.log.bind(console, type);
-const onSubmit = ({formData}, e) => console.log("Data submitted: ",  formData);
+const onSubmit = ({formData}, e) => console.log("Data submitted: ",  JSON.stringify(formData));
 
 
 class App extends React.Component {
