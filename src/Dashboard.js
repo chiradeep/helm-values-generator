787c44f25d167d07d450ce9c307b494c974b8c62
Chiradeep Vittal <chiradeepv@gmail.com>
2020-07-24 11:25:16 -0700

    try out sub menus

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,125 +1,145 @@
 import React from 'react';
 import SidebarItems from './SidebarItems';
-import {Sidebar, Menu, Grid, Segment, Item, Header} from 'semantic-ui-react';
+import { Sidebar, Menu, Grid, Segment, Item, Header } from 'semantic-ui-react';
 import HelmForm from './HelmForm';
-import {tier1schema, tier1uischema} from './chart-tier-1';
-import {cpxcicschema, cpxcicuischema} from './cpx-cic';
+import { tier1schema, tier1uischema } from './chart-tier-1';
+import { cpxcicschema, cpxcicuischema } from './cpx-cic';
 
 function Copyright() {
-  return (
-    <Segment attached={'bottom'} black basic padded className='ut_border_none' style={{marginLeft: '1.4rem'}}>
-      {'Copyright © '}
-      <Item as={'a'} href="https://citrix.com/">
-        Citrix Systems
+    return (
+        <Segment attached={'bottom'} black basic padded className='ut_border_none' style={{ marginLeft: '1.4rem' }}>
+            {'Copyright © '}
+            <Item as={'a'} href="https://citrix.com/">
+                Citrix Systems
       </Item>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Segment>
-  );
+            {new Date().getFullYear()}
+            {'.'}
+        </Segment>
+    );
 }
 
 export default function Dashboard() {
-  const items = ['Citrix ADC in Tier 1', 'CPX in Tier 2', 'Istio Ingress Gateway', 'CPX as Istio Sidecar'];
+    const items = ['Citrix ADC in Tier 1', 'CPX in Tier 2', 'Istio Ingress Gateway', 'CPX as Istio Sidecar'];
 
-  const [formId, setformId] = React.useState('Citrix ADC in Tier 1');
-  /*const [open, setOpen] = React.useState(true);
-  const handleDrawerOpen = () => {
-    setOpen(true);
-  };
-  const handleDrawerClose = () => {
-    setOpen(false);
-  };*/
+    const config = [
+        {
+            'title': 'Unified Ingress',
+            'items': ['Citrix ADC in Tier 1', 'Service Type LB']
+        },
+        {
+            'title': '2 Tier Ingress',
+            'items': ['CPX in Tier 2', 'Service Mesh Lite']
 
-  const itemClick = (itemName, e) => {
-    console.log('Clicked on ' + itemName);
-    setformId(itemName);
-  }
+        },
+        {
+            'title': 'Service Mesh',
+            'items': ['Istio Ingress Gateway', 'CPX as Istio Sidecar']
+        },
+        {
+            'title': 'Components',
+            'items': ['Citrix Observability Exporter', 'Citrix Node Controller']
+        }
+    ]
 
-  const [formData] = React.useState({[items[0]]: {}, [items[1]]: {}, [items[2]]: {}, [items[3]]: {}});
-  const [yamlStrs] = React.useState({[items[0]]: '', [items[1]]: '', [items[2]]: '', [items[3]]: ''});
+    const [formId, setformId] = React.useState('Citrix ADC in Tier 1');
+    /*const [open, setOpen] = React.useState(true);
+    const handleDrawerOpen = () => {
+      setOpen(true);
+    };
+    const handleDrawerClose = () => {
+      setOpen(false);
+    };*/
 
-  //let formData  = {[items[0]]: {}, [items[1]]: {}, [items[2]]: {}, [items[3]]: {}};
-  //let yamlStrs  = {[items[0]]: '', [items[1]]: '', [items[2]]: '', [items[3]]: ''};
-
-  const setParentState = (formId, data, yamlStr) => {
-    //setFormData(prevFormData => ({...prevFormData, [formData[formId]]:data}));
-    //setYamlStrs(prevYamlStrs =>({...prevYamlStrs, [prevYamlStrs[formId]]: yamlStr}));
-    //formData = {...formData, [formData[formId]]:data};
-    formData[formId] = data;
-    yamlStrs[formId] = yamlStr;
-    console.log('Form id= ' + formId);
-    console.log('Form data= ', formData[formId]);
-    console.log('Yaml: ', yamlStrs[formId]);
-  }
-
-  const schemaForForm = (formId) => {
-    //TODO
-    switch (formId) {
-      case items[0]:
-        return tier1schema;
-      case items[1]:
-        return cpxcicschema;
-      default:
-        return tier1schema;
+    const itemClick = (itemName, e) => {
+        console.log('Clicked on ' + itemName);
+        setformId(itemName);
     }
-  }
 
-  const uischemaForForm = (formId) => {
-    //TODO
-    switch (formId) {
-      case items[0]:
-        return tier1uischema;
-      case items[1]:
-        return cpxcicuischema;
-      default:
-        return tier1uischema;
+    const [formData] = React.useState({ [items[0]]: {}, [items[1]]: {}, [items[2]]: {}, [items[3]]: {} });
+    const [yamlStrs] = React.useState({ [items[0]]: '', [items[1]]: '', [items[2]]: '', [items[3]]: '' });
+
+    //let formData  = {[items[0]]: {}, [items[1]]: {}, [items[2]]: {}, [items[3]]: {}};
+    //let yamlStrs  = {[items[0]]: '', [items[1]]: '', [items[2]]: '', [items[3]]: ''};
+
+    const setParentState = (formId, data, yamlStr) => {
+        //setFormData(prevFormData => ({...prevFormData, [formData[formId]]:data}));
+        //setYamlStrs(prevYamlStrs =>({...prevYamlStrs, [prevYamlStrs[formId]]: yamlStr}));
+        //formData = {...formData, [formData[formId]]:data};
+        formData[formId] = data;
+        yamlStrs[formId] = yamlStr;
+        console.log('Form id= ' + formId);
+        console.log('Form data= ', formData[formId]);
+        console.log('Yaml: ', yamlStrs[formId]);
     }
-  }
 
-  const MainContent = ({formId, formData, yamlStr}) => {
+    const schemaForForm = (formId) => {
+        //TODO
+        switch (formId) {
+            case items[0]:
+                return tier1schema;
+            case items[1]:
+                return cpxcicschema;
+            default:
+                return tier1schema;
+        }
+    }
+
+    const uischemaForForm = (formId) => {
+        //TODO
+        switch (formId) {
+            case items[0]:
+                return tier1uischema;
+            case items[1]:
+                return cpxcicuischema;
+            default:
+                return tier1uischema;
+        }
+    }
+
+    const MainContent = ({ formId, formData, yamlStr }) => {
+        return (
+            <>
+
+                <Grid.Row>
+                    <HelmForm schema={schemaForForm(formId)}
+                        uischema={uischemaForForm(formId)}
+                        formData={formData}
+                        yamlStr={yamlStr}
+                        setParentState={setParentState}
+                    />
+                </Grid.Row>
+            </>
+        );
+    }
+
     return (
-      <>  
-     
-      <Grid.Row>
-          <HelmForm schema={schemaForForm(formId)}
-                    uischema={uischemaForForm(formId)}
-                    formData={formData}
-                    yamlStr={yamlStr}
-                    setParentState={setParentState}
-          />
-       </Grid.Row>
+        <>
+            <Header inverted attached={'top'} as={'h1'} className='mastHead'>Citrix Helm Charts Values Generator</Header>
+            <Sidebar.Pushable attached={'top'} basic as={Segment} style={{ marginTop: '0', borderRadius: '0', borderTop: '0', borderBottom: '0' }}>
+                <Sidebar
+                    // style={{top: '-1px'}}
+                    as={Menu}
+                    inverted={false}
+                    vertical
+                    visible={true}
+                    width='wide'
+                >
+                    <SidebarItems config={config} onClick={itemClick} />
+                </Sidebar>
+                <Sidebar.Pusher>
+                    <Header className='titleHeader ut_border_none' as={'h3'} attached={'top'} >{formId}</Header>
+                    <Grid className='mainlayout'>
+                        <MainContent
+                            formId={formId}
+                            formData={formData[formId]}
+                            yamlStr={yamlStrs[formId]}
+                            setParentState={setParentState}
+                        />
+                    </Grid>
+                    <Copyright />
+                </Sidebar.Pusher>
+            </Sidebar.Pushable>
         </>
     );
-  }
-
-  return (
-    <>
-    <Header inverted attached={'top'} as={'h1'} className='mastHead'>Citrix Helm Charts Values Generator</Header>
-    <Sidebar.Pushable attached={'top'} basic as={Segment} style={{marginTop: '0', borderRadius: '0', borderTop: '0', borderBottom: '0'}}>
-      <Sidebar
-        // style={{top: '-1px'}}
-        as={Menu}
-        inverted={false}
-        vertical
-        visible={true}
-        width='wide'
-      >
-        <SidebarItems items={items} onClick={itemClick}/>
-      </Sidebar>
-      <Sidebar.Pusher>
-    <Header className='titleHeader ut_border_none' as={'h3'} attached={'top'} >{formId}</Header>
-        <Grid className='mainlayout'>
-          <MainContent
-            formId={formId}
-            formData={formData[formId]}
-            yamlStr={yamlStrs[formId]}
-            setParentState={setParentState}
-          />
-        </Grid>
-        <Copyright/>
-      </Sidebar.Pusher>
-    </Sidebar.Pushable>
-      </>
-  );
 }
 
--- src/SidebarItems.js
+++ src/SidebarItems.js
@@ -1,23 +1,47 @@
 import React from 'react';
-import { Menu } from 'semantic-ui-react';
+import { Accordion, Menu, Header } from 'semantic-ui-react';
 
 
 
 class SidebarItems extends React.Component {
-  render() {
+    state = { activeIndex: 0 }
+
+    handleClick = (e, titleProps) => {
+        const { index } = titleProps
+        const { activeIndex } = this.state
+        const newIndex = activeIndex === index ? -1 : index
+
+        this.setState({ activeIndex: newIndex })
+    }
+
+    render() {
+        const { activeIndex } = this.state;
+        var i = 0;
         return (
-          <>
-          {this.props.items.map(((item) => {
-            return (
-              <Menu.Item as='a' key={item} onClick={(e) => this.props.onClick(item, e)}>
-                {item} 
-              </Menu.Item>
-            );
-          }))}
-          </>
+            <Accordion as={Menu} vertical secondary fluid exclusive={false}>
+                {this.props.config.map(((cfg) => {
+                    i++;
+                    return (
+                    <Menu.Item position='left'>
+                        <Accordion.Title
+                            active={activeIndex === i}
+                            content={<Header as='h4'>{cfg.title}</Header>}
+                            index={i}
+                            onClick={this.handleClick}
+                            left
+                        />
+                        {cfg.items.map(((item) => {
+                            return (
+                                <Accordion.Content active={activeIndex === i} content={item} />
+                            );
+                        }))}
+                    </Menu.Item>
+                    );
+                }))}
+            </Accordion>
         );
-          
-  }
+
+    }
 }
 
 export default SidebarItems;
